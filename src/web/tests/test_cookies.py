731a323ea234283550d81c6a502569811e7137b0
Dmitry Dorofeev <dorofeev@game-insight.com>
2017-03-24 19:53:34 +0300

    test fixed

--- src/web/tests/test_cookies.py
+++ src/web/tests/test_cookies.py
@@ -3,8 +3,8 @@ from web import cookies
 
 def test_encode_decode():
     data = {'k': 123, 's': 'hello'}
-    encoded_cookies = cookies.encode(data)
-    d = cookies.decode(encoded_cookies)
+    encoded_cookies = cookies.encode(data, key='123')
+    d = cookies.decode(encoded_cookies, key='123')
     assert d['k'] == 123
     assert d['s'] == 'hello'
     assert len(d.keys()) == 2
