ad6602717b57b27f81338f47e97cd5b22a5d44d6
Dmitry Dorofeev <djudman@gmail.com>
2017-03-26 17:01:56 +0300

    downloader speed up

--- src/bot/downloader.py
+++ src/bot/downloader.py
@@ -29,7 +29,7 @@ class TelegramDownloader(HttpDownloader):
             try:
                 tasks = self.download_all()
                 if not tasks:
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(0.05)
             except Exception as e:
                 self.logger.error(e, exc_info=1)
 
