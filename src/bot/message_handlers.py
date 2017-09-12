bcedf05e7c37404b1789a4a8f75da764f490a5e6
Dmitry Dorofeev <dorofeev@game-insight.com>
2017-09-12 18:46:11 +0300

    fix

--- src/bot/message_handlers.py
+++ src/bot/message_handlers.py
@@ -133,7 +133,10 @@ class FileHandler(BaseHandler):
     async def get_files(self, message: Message):
         file_id = self.get_file_id(message)
         file_path, mime_type = await self.downloader.download_file(file_id)
-        destination_file = os.path.join(os.path.dirname(file_path), message.document.file_name)
+        if message.document:
+            destination_file = os.path.join(os.path.dirname(file_path), message.document.file_name)
+        else:
+            destination_file = file_path
         os.rename(file_path, destination_file)
         return [(destination_file, mime_type)]
 
