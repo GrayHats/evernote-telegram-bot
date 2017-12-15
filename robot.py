840fe4f56af998c9069c45c7662b24f6e4c07b69
Dmitry Dorofeev <dorofeev@game-insight.com>
2017-12-15 16:49:52 +0300

    cleanup

--- robot.py
+++ robot.py
@@ -18,6 +18,7 @@ sys.path.append(join(realpath(dirname(__file__)), 'src'))
 
 from config import config
 from utils.daemon import Daemon
+from bot.dealer import EvernoteDealerDaemon
 
 
 def green(text):
@@ -38,46 +39,24 @@ class BotDaemon(Daemon):
         aiohttp.web.run_app(app, port=config['port'])
 
 
-class ProcessOwner:
-    def run(self, exec_path, *args):
-        print('Running {}...'.format(basename(exec_path)), end='')
-        process = subprocess.run([exec_path, *args],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT)
-        if process.returncode != 0:
-            print(red('FAILED'))
-            raise Exception(process.stdout.decode())
-        print(green('OK'))
+class BotService:
+    def __init__(self):
+        self.config = config
 
-    def kill(self, pidfile, signal=signal.SIGTERM, message=None):
-        if message is None:
-            message = 'Killing {}...'.format(basename(pidfile))
-        print(message, end='')
-        if os.path.exists(pidfile):
-            if self.process_exists(pidfile):
-                with open(pidfile) as f:
-                    pid = int(f.read())
-                os.kill(pid, signal)
-                if signal == signal.SIGTERM:
-                    os.unlink(pidfile)
-                print(green('OK'))
-            else:
-                print(red('FAILED'))
-                print(red('Process {} not runnning'.format(pidfile)))
-        else:
-            print(green('OK'))
+        project_dir = self.config['project_dir']
+        dealer_pidfile = join(project_dir, 'dealer.pid')
+        dealer_stdout = join(project_dir, 'dealer.stdout.log')
+        self.dealer_daemon = EvernoteDealerDaemon(dealer_pidfile, dealer_stdout)
 
-    def process_exists(self, pidfile):
-        if not os.path.exists(pidfile):
-            return False
-        with open(pidfile) as f:
-            pid = int(f.read())
-            try:
-                os.kill(pid, 0)
-            except OSError:
-                os.unlink(pidfile)
-                return False
-        return True
+        bot_pidfile = join(project_dir, 'bot.pid')
+        bot_stdout = join(project_dir, 'bot.stdout.log')
+        self.bot_daemon = BotDaemon(bot_pidfile, bot_stdout)
+
+    def start(self):
+        os.makedirs(self.config['logs_dir'], mode=0o700, exist_ok=True)
+        os.makedirs(self.config['downloads_dir'], mode=0o700, exist_ok=True)
+        self.daemon_start(self.dealer_daemon.start())
+        self.daemon_start(self.bot_daemon.start())
 
     def daemon_start(self, daemon):
         print('Starting {}...'.format(basename(daemon.pidfile)))
@@ -88,6 +67,10 @@ class ProcessOwner:
             print(red('FAILED'))
             raise e
 
+    def stop(self):
+        self.daemon_stop(self.dealer_daemon)
+        self.daemon_stop(self.bot_daemon)
+
     def daemon_stop(self, daemon):
         print('Stopping {}...'.format(basename(daemon.pidfile)), end='')
         try:
@@ -96,29 +79,6 @@ class ProcessOwner:
         except Exception as e:
             print(red('FAILED'))
             raise e
-
-
-class BotService(ProcessOwner):
-    def __init__(self):
-        self.config = config
-
-        dealer_module = importlib.import_module('bot.dealer')
-        dealer_pidfile = join(self.config['project_dir'], 'dealer.pid')
-        dealer_stdout = join(self.config['project_dir'], 'dealer.stdout.log')
-        self.dealer_daemon = dealer_module.EvernoteDealerDaemon(dealer_pidfile, dealer_stdout)
-        bot_pidfile = join(self.config['project_dir'], 'bot.pid')
-        bot_stdout = join(self.config['project_dir'], 'bot.stdout.log')
-        self.bot_daemon = BotDaemon(bot_pidfile, bot_stdout)
-
-    def start(self):
-        os.makedirs(self.config['logs_dir'], mode=0o700, exist_ok=True)
-        os.makedirs(self.config['downloads_dir'], mode=0o700, exist_ok=True)
-        self.daemon_start(self.dealer_daemon)
-        self.daemon_start(self.bot_daemon)
-
-    def stop(self):
-        self.daemon_stop(self.dealer_daemon)
-        self.daemon_stop(self.bot_daemon)
 
     def restart(self):
         self.stop()
